97eb6ef95a828e52f45d9eaec78be70da38adf2f
derrrick william <williamderrick100@gmail.com>
2018-12-03 08:39:55 +0300

    Corrected multiple errors on the file

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-function Triangle() {
+function triangle(){
   var side1 = document.getElementById("length1").value;
   var side2 = document.getElementById("length2").value;
   var side3 = document.getElementById("length3").value;
@@ -12,4 +12,7 @@ function Triangle() {
   } else if ((side1 + side2 > side3 || side2 + side3 > side1 || side3 + side1 > side2) & (side1 != side2 && side2 != side3 && side3 != side1)) {
     alert("Scalene Triangle");
   }
+}
+function reload(){
+  location.reload();
 }
