b8f38bb7fe159b7b6c091dbfa4692cb71a02c9de
derrrick william <williamderrick100@gmail.com>
2018-12-03 08:06:08 +0300

    Created a Button and corrected errors on the function

--- js/script.js
+++ js/script.js
@@ -1,10 +1,15 @@
-function Triangle () {
-    var side1 = application.getElementById('length1');
-    var side2 = application.get
-    if (side1 == side2 && side2 == side3 && side3 == side1){
-      return = "Equilateral Triangle";
-    } else if (side1 || side2 ||side2 == side3 ||side1){return "Isosceles Triangle";
-  }else if ((side1 + side2 > side3 || side2 + side3 > side1 || side3 + side1 > side2)&(side1 != side2 && side2 !=side3 && side3 != side1))
+function Triangle() {
+  var side1 = document.getElementById("length1").value;
+  var side2 = document.getElementById("length2").value;
+  var side3 = document.getElementById("length3").value;
 
-    }
+  if (side1 == side2 && side2 == side3 && side3 == side1) {
+    alert("Equilateral Triangle");
+
+  } else if (side1 || side2 || side2 == side3 || side1) {
+    alert("Isosceles Triangle");
+
+  } else if ((side1 + side2 > side3 || side2 + side3 > side1 || side3 + side1 > side2) & (side1 != side2 && side2 != side3 && side3 != side1)) {
+    alert("Scalene Triangle");
+  }
 }
