65370ede172cdc68fdbda58d77e0366fbae7351a
derrrick william <williamderrick100@gmail.com>
2018-12-03 09:27:49 +0300

    Made corrections on Js file

--- js/script.js
+++ js/script.js
@@ -3,18 +3,26 @@ function triangle(){
   var side2 = document.getElementById("length2").value;
   var side3 = document.getElementById("length3").value;
 
+if (side1 > 0 && side2 > 0 && side3 > 0){
   if (side1 == side2 && side2 == side3 && side3 == side1) {
     alert("Equilateral Triangle");
-
-  } else if (side1 || side2 || side2 == side3 || side1) {
+}  else if(isNaN(side1)||isNaN(side2)||isNaN(side3)){
+      alert("invalid entry");
+}
+   else if (side1 || side2 || side2 == side3 || side1) {
     alert("Isosceles Triangle");
 
   } else if ((side1 + side2 > side3 || side2 + side3 > side1 || side3 + side1 > side2) & (side1 != side2 && side2 != side3 && side3 != side1)) {
     alert("Scalene Triangle");
-  } else if (side1 + side2 <= sede3 || side2 + side3 <= side1 ||side3 + side1 <= side2) {
-  alert("Not a Triangle")    
+  } else if (side1 + side2 <= side3 || side2 + side3 <= side1 ||side3 + side1 <= side2) {
+  alert("Not a Triangle");
   }
+
+} else {
+  alert("Value has to be greater than zero");
 }
+
+};
 function reload(){
   location.reload();
 }
