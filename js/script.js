5323811e41f085f420b9f38b8aec51dacad86267
derrrick william <williamderrick100@gmail.com>
2018-12-03 07:42:29 +0300

    Added conditions to the js file

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
 function Triangle () {
-    var side1 = prompt("Enter the size number");
+    var side1 = application.getElementById('length1');
+    var side2 = application.get
+    if (side1 == side2 && side2 == side3 && side3 == side1){
+      return = "Equilateral Triangle";
+    } else if (side1 || side2 ||side2 == side3 ||side1){return "Isosceles Triangle";
+  }else if ((side1 + side2 > side3 || side2 + side3 > side1 || side3 + side1 > side2)&(side1 != side2 && side2 !=side3 && side3 != side1))
+
+    }
 }
